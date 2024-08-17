9a83f9e6c843534cc6d4cebe0617df32474f7f5c
7irelo <tirelo.eric@gmail.com>
2024-08-17 14:51:49 +0200

    Update sign_in_form.h

--- Sign_in/sign_in_form.h
+++ Sign_in/sign_in_form.h
@@ -1,5 +1,11 @@
 #pragma once
 
+#include <msclr/marshal_cppstd.h>
+#include <System/Net/WebClient.h>
+#include <System/Text/UTF8Encoding.h>
+#include <System/Net/HttpWebRequest.h>
+#include <System/Net/HttpWebResponse.h>
+
 namespace Signin {
 
 	using namespace System;
@@ -8,6 +14,9 @@ namespace Signin {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
+	using namespace System::Net;
+	using namespace System::IO;
+	using namespace System::Text;
 
 	public ref class sign_in_form : public System::Windows::Forms::Form
 	{
@@ -143,20 +152,19 @@ namespace Signin {
 			this->panel2->Location = System::Drawing::Point(38, 268);
 			this->panel2->Name = L"panel2";
 			this->panel2->Size = System::Drawing::Size(230, 2);
-			this->panel2->TabIndex = 6;
+			this->panel2->TabIndex = 7;
 			// 
 			// textBox2
 			// 
 			this->textBox2->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(162)), static_cast<System::Int32>(static_cast<System::Byte>(163)),
 				static_cast<System::Int32>(static_cast<System::Byte>(163)));
 			this->textBox2->BorderStyle = System::Windows::Forms::BorderStyle::None;
-			this->textBox2->Font = (gcnew System::Drawing::Font(L"Bahnschrift Light", 9.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
+			this->textBox2->Font = (gcnew System::Drawing::Font(L"Bahnschrift Light", 9.75F));
 			this->textBox2->Location = System::Drawing::Point(38, 251);
 			this->textBox2->Name = L"textBox2";
+			this->textBox2->PasswordChar = '*';
 			this->textBox2->Size = System::Drawing::Size(230, 16);
-			this->textBox2->TabIndex = 5;
-			this->textBox2->UseSystemPasswordChar = true;
+			this->textBox2->TabIndex = 6;
 			this->textBox2->KeyDown += gcnew System::Windows::Forms::KeyEventHandler(this, &sign_in_form::textBox2_KeyDown);
 			// 
 			// checkBox1
@@ -164,239 +172,152 @@ namespace Signin {
 			this->checkBox1->AutoSize = true;
 			this->checkBox1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(162)), static_cast<System::Int32>(static_cast<System::Byte>(163)),
 				static_cast<System::Int32>(static_cast<System::Byte>(163)));
-			this->checkBox1->Font = (gcnew System::Drawing::Font(L"Bahnschrift", 8.75F));
-			this->checkBox1->Location = System::Drawing::Point(37, 286);
+			this->checkBox1->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->checkBox1->Font = (gcnew System::Drawing::Font(L"Bahnschrift", 7.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->checkBox1->Location = System::Drawing::Point(38, 302);
 			this->checkBox1->Name = L"checkBox1";
-			this->checkBox1->Size = System::Drawing::Size(95, 18);
-			this->checkBox1->TabIndex = 7;
-			this->checkBox1->Text = L"I agree to the";
+			this->checkBox1->Size = System::Drawing::Size(145, 17);
+			this->checkBox1->TabIndex = 8;
+			this->checkBox1->Text = L"I agree to the terms of use.";
 			this->checkBox1->UseVisualStyleBackColor = false;
-			this->checkBox1->CheckedChanged += gcnew System::EventHandler(this, &sign_in_form::checkBox1_CheckedChanged);
 			// 
 			// label4
 			// 
 			this->label4->AutoSize = true;
 			this->label4->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(162)), static_cast<System::Int32>(static_cast<System::Byte>(163)),
 				static_cast<System::Int32>(static_cast<System::Byte>(163)));
-			this->label4->Cursor = System::Windows::Forms::Cursors::Hand;
-			this->label4->Font = (gcnew System::Drawing::Font(L"Bahnschrift", 8.75F));
-			this->label4->ForeColor = System::Drawing::Color::Blue;
-			this->label4->Location = System::Drawing::Point(128, 287);
+			this->label4->Font = (gcnew System::Drawing::Font(L"Bahnschrift", 7.8F, System::Drawing::FontStyle::Underline, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label4->Location = System::Drawing::Point(189, 302);
 			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(121, 14);
-			this->label4->TabIndex = 8;
-			this->label4->Text = L"Terms and Conditions";
+			this->label4->Size = System::Drawing::Size(79, 13);
+			this->label4->TabIndex = 9;
+			this->label4->Text = L"Read terms here.";
 			this->label4->Click += gcnew System::EventHandler(this, &sign_in_form::label4_Click);
 			// 
 			// button1
 			// 
-			this->button1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(64)), static_cast<System::Int32>(static_cast<System::Byte>(64)),
-				static_cast<System::Int32>(static_cast<System::Byte>(64)));
-			this->button1->Enabled = false;
+			this->button1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(29)), static_cast<System::Int32>(static_cast<System::Byte>(29)),
+				static_cast<System::Int32>(static_cast<System::Byte>(29)));
 			this->button1->FlatAppearance->BorderSize = 0;
 			this->button1->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
-			this->button1->Font = (gcnew System::Drawing::Font(L"Bahnschrift", 10.75F));
+			this->button1->Font = (gcnew System::Drawing::Font(L"Bahnschrift Light", 10.2F));
 			this->button1->ForeColor = System::Drawing::Color::White;
-			this->button1->Location = System::Drawing::Point(38, 319);
+			this->button1->Location = System::Drawing::Point(40, 354);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(112, 34);
-			this->button1->TabIndex = 9;
-			this->button1->Text = L"Sign in";
+			this->button1->Size = System::Drawing::Size(230, 39);
+			this->button1->TabIndex = 10;
+			this->button1->Text = L"Continue";
 			this->button1->UseVisualStyleBackColor = false;
 			this->button1->Click += gcnew System::EventHandler(this, &sign_in_form::button1_Click);
 			// 
 			// button2
 			// 
-			this->button2->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(162)), static_cast<System::Int32>(static_cast<System::Byte>(163)),
-				static_cast<System::Int32>(static_cast<System::Byte>(163)));
+			this->button2->BackColor = System::Drawing::Color::Transparent;
+			this->button2->FlatAppearance->BorderSize = 0;
 			this->button2->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
-			this->button2->Font = (gcnew System::Drawing::Font(L"Bahnschrift", 10.75F));
-			this->button2->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(64)), static_cast<System::Int32>(static_cast<System::Byte>(64)),
-				static_cast<System::Int32>(static_cast<System::Byte>(64)));
-			this->button2->Location = System::Drawing::Point(156, 319);
+			this->button2->Font = (gcnew System::Drawing::Font(L"Bahnschrift Light", 10.2F));
+			this->button2->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(1)), static_cast<System::Int32>(static_cast<System::Byte>(1)),
+				static_cast<System::Int32>(static_cast<System::Byte>(1)));
+			this->button2->Location = System::Drawing::Point(40, 399);
 			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(112, 34);
-			this->button2->TabIndex = 10;
-			this->button2->Text = L"Exit";
+			this->button2->Size = System::Drawing::Size(230, 39);
+			this->button2->TabIndex = 11;
+			this->button2->Text = L"Create an account";
 			this->button2->UseVisualStyleBackColor = false;
-			this->button2->Click += gcnew System::EventHandler(this, &sign_in_form::button2_Click);
 			// 
 			// panelTerms
 			// 
-			this->panelTerms->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(162)), static_cast<System::Int32>(static_cast<System::Byte>(163)),
-				static_cast<System::Int32>(static_cast<System::Byte>(163)));
-			this->panelTerms->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			this->panelTerms->BackColor = System::Drawing::Color::White;
 			this->panelTerms->Controls->Add(this->button3);
 			this->panelTerms->Controls->Add(this->textBox3);
 			this->panelTerms->Controls->Add(this->label5);
-			this->panelTerms->Dock = System::Windows::Forms::DockStyle::Fill;
-			this->panelTerms->Font = (gcnew System::Drawing::Font(L"Bahnschrift", 8));
-			this->panelTerms->Location = System::Drawing::Point(0, 0);
+			this->panelTerms->Location = System::Drawing::Point(300, 12);
 			this->panelTerms->Name = L"panelTerms";
-			this->panelTerms->Size = System::Drawing::Size(727, 477);
-			this->panelTerms->TabIndex = 11;
-			this->panelTerms->MouseDown += gcnew System::Windows::Forms::MouseEventHandler(this, &sign_in_form::sign_in_form_MouseDown);
-			this->panelTerms->MouseMove += gcnew System::Windows::Forms::MouseEventHandler(this, &sign_in_form::sign_in_form_MouseMove);
-			this->panelTerms->MouseUp += gcnew System::Windows::Forms::MouseEventHandler(this, &sign_in_form::sign_in_form_MouseUp);
+			this->panelTerms->Size = System::Drawing::Size(355, 426);
+			this->panelTerms->TabIndex = 12;
 			// 
 			// button3
 			// 
-			this->button3->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(64)), static_cast<System::Int32>(static_cast<System::Byte>(64)),
-				static_cast<System::Int32>(static_cast<System::Byte>(64)));
-			this->button3->FlatAppearance->BorderSize = 0;
+			this->button3->BackColor = System::Drawing::Color::Black;
 			this->button3->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
-			this->button3->Font = (gcnew System::Drawing::Font(L"Bahnschrift", 10.75F));
+			this->button3->Font = (gcnew System::Drawing::Font(L"Bahnschrift Light", 10.2F));
 			this->button3->ForeColor = System::Drawing::Color::White;
-			this->button3->Location = System::Drawing::Point(392, 412);
+			this->button3->Location = System::Drawing::Point(29, 354);
 			this->button3->Name = L"button3";
-			this->button3->Size = System::Drawing::Size(112, 34);
-			this->button3->TabIndex = 10;
-			this->button3->Text = L"OK";
+			this->button3->Size = System::Drawing::Size(299, 39);
+			this->button3->TabIndex = 2;
+			this->button3->Text = L"Back";
 			this->button3->UseVisualStyleBackColor = false;
 			this->button3->Click += gcnew System::EventHandler(this, &sign_in_form::button3_Click);
 			// 
 			// textBox3
 			// 
-			this->textBox3->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(162)), static_cast<System::Int32>(static_cast<System::Byte>(163)),
-				static_cast<System::Int32>(static_cast<System::Byte>(163)));
-			this->textBox3->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
-			this->textBox3->Font = (gcnew System::Drawing::Font(L"Bahnschrift", 8));
-			this->textBox3->ForeColor = System::Drawing::Color::Black;
-			this->textBox3->Location = System::Drawing::Point(59, 85);
+			this->textBox3->BackColor = System::Drawing::Color::White;
+			this->textBox3->BorderStyle = System::Windows::Forms::BorderStyle::None;
+			this->textBox3->Font = (gcnew System::Drawing::Font(L"Bahnschrift Light", 9.75F));
+			this->textBox3->Location = System::Drawing::Point(15, 44);
 			this->textBox3->Multiline = true;
-			this->textBox3->Name = L"textBox3";
+			this->textBox3->Name = L"textBox3;
 			this->textBox3->ReadOnly = true;
-			this->textBox3->Size = System::Drawing::Size(445, 300);
+			this->textBox3->Size = System::Drawing::Size(329, 280);
 			this->textBox3->TabIndex = 1;
 			this->textBox3->Text = resources->GetString(L"textBox3.Text");
 			// 
 			// label5
 			// 
 			this->label5->AutoSize = true;
-			this->label5->Font = (gcnew System::Drawing::Font(L"Bahnschrift", 15.75F));
-			this->label5->Location = System::Drawing::Point(54, 45);
+			this->label5->Font = (gcnew System::Drawing::Font(L"Bahnschrift Light", 18));
+			this->label5->Location = System::Drawing::Point(3, 0);
 			this->label5->Name = L"label5";
-			this->label5->Size = System::Drawing::Size(214, 25);
+			this->label5->Size = System::Drawing::Size(139, 29);
 			this->label5->TabIndex = 0;
-			this->label5->Text = L"Terms and Conditions";
+			this->label5->Text = L"Terms of use";
 			// 
 			// sign_in_form
 			// 
-			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::None;
-			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
-			this->ClientSize = System::Drawing::Size(727, 477);
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::Color::White;
+			this->ClientSize = System::Drawing::Size(684, 461);
 			this->Controls->Add(this->panelTerms);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->label4);
 			this->Controls->Add(this->checkBox1);
-			this->Controls->Add(this->panel2);
 			this->Controls->Add(this->textBox2);
-			this->Controls->Add(this->panel1);
-			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->panel2);
 			this->Controls->Add(this->label3);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->panel1);
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->label1);
-			this->DoubleBuffered = true;
-			this->Font = (gcnew System::Drawing::Font(L"Segoe UI Semilight", 15, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->ForeColor = System::Drawing::Color::Black;
-			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
-			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedDialog;
+			this->MaximizeBox = false;
+			this->MinimizeBox = false;
 			this->Name = L"sign_in_form";
-			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
-			this->Text = L"Sign in";
-			this->MouseDown += gcnew System::Windows::Forms::MouseEventHandler(this, &sign_in_form::sign_in_form_MouseDown);
-			this->MouseMove += gcnew System::Windows::Forms::MouseEventHandler(this, &sign_in_form::sign_in_form_MouseMove);
-			this->MouseUp += gcnew System::Windows::Forms::MouseEventHandler(this, &sign_in_form::sign_in_form_MouseUp);
+			this->Text = L"Sign In";
 			this->panelTerms->ResumeLayout(false);
 			this->panelTerms->PerformLayout();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
-
-
 #pragma endregion
-	
-		private: System::Void checkBox1_CheckedChanged(System::Object^ sender, System::EventArgs^ e)
-		{
-			if (!checkBox1->Enabled)
-			{
-				button1->Enabled = false;
-			}
-			else
-			{
-				button1->Enabled = true;
-			}
+	private: System::Void label4_Click(System::Object^ sender, System::EventArgs^ e) {
+		this->panelTerms->BringToFront();
+	}
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		// Sign in logic goes here
+	}
+	private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
+		this->panelTerms->SendToBack();
+	}
+	private: System::Void textBox2_KeyDown(System::Object^ sender, System::Windows::Forms::KeyEventArgs^ e) {
+		if (e->KeyCode == System::Windows::Forms::Keys::Enter) {
+			// Sign in logic goes here
 		}
-		private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e)
-		{
-			Application::Exit();
-		}
-		private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e)
-		{
-			if (textBox1->Text == "admin")
-			{
-				if (textBox2->Text == "root")
-				{
-					MessageBox::Show("Access Granted", "Sign in Successful", MessageBoxButtons::OK, MessageBoxIcon::Information);
-					Application::Exit();
-				}
-				else
-				{
-					MessageBox::Show("Incorrect Password", "Error", MessageBoxButtons::OK, MessageBoxIcon::Error);
-				}
-			}
-			else
-			{
-				MessageBox::Show("Incorrect Username", "Error", MessageBoxButtons::OK, MessageBoxIcon::Error);
-			}
-		}
-		private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e)
-		{
-			panelTerms->Hide();
-		}
-
-		
-		private: System::Void label4_Click(System::Object^ sender, System::EventArgs^ e)
-		{
-			panelTerms->Show();
-		}
-		
-		bool dragging;
-		Point offset;
-		private: System::Void sign_in_form_MouseDown(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e)
-		{
-			dragging = true;
-			offset.X = e->X;
-			offset.Y = e->Y;
-		}
-		private: System::Void sign_in_form_MouseMove(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e)
-		{
-			if (dragging)
-			{
-				Point currentScreenPosition = PointToScreen(Point(e->X, e->Y));
-				Location = Point(currentScreenPosition.X - offset.X, currentScreenPosition.Y - offset.Y);
-			}
-		}
-		private: System::Void sign_in_form_MouseUp(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e)
-		{
-			dragging = false;
-		}
-		private: System::Void textBox1_KeyDown(System::Object^ sender, System::Windows::Forms::KeyEventArgs^ e)
-		{
-			if (e->KeyValue == (int)Keys::Enter)
-			{
-				textBox2->Focus();
-			}
-		}
-		private: System::Void textBox2_KeyDown(System::Object^ sender, System::Windows::Forms::KeyEventArgs^ e)
-		{
-			if (e->KeyValue == (int)Keys::Enter)
-			{
-				button1->PerformClick();
-			}
-		}
-	};
+	}
+};
 }
